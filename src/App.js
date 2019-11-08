f0f16af485318b2d72d27c9d9754991d5b642310
Claud Pod <claudiapollinger.cp@gmail.com>
2019-11-08 14:04:40 -0700

    added json, to Header, App, index

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import './App.css';
+import Header from "./components/Header"
+
+render() {
+    return (
+        <div className="App">
+            <Header />
+        </div>
+    );
+}
+
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header () {
+    return (
+        <div ClassName ="header">
+            <h1> Claudia Pollinger</h1>
+            </div>
+    )
+};
+
+export default Header; 
